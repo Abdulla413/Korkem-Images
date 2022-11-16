8281b5a8a0d3d502dc5b63e22509d2e041801e52
abdulla <abdulla.arish@protonmail.com>
2022-11-16 22:30:11 +0100

    ready to deployment

--- server/server.js
+++ server/server.js
@@ -150,8 +150,17 @@ app.post("/api/image/delete", (req, res) => {
         });
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(`${__dirname}/index.html`);
-});
 
-app.listen(5000, () => console.log(`I'm listening posrt 5000.`));
+
+if(process.env.NODE_ENV==="production"){
+    //Static folder
+    app.use(express.static(__dirname + '/public/'));
+    // Handle SPA
+    app.get("*", (req, res) => {
+        res.sendFile(`${__dirname}/public/index.html`);
+    });
+}
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`I'm listening posrt ${port}`));
